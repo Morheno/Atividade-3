748a88874b0938dadcbd9b06c40bdee3495d51c2
Morheno <morheno@gmail.com>
2024-09-16 14:25:50 -0300

    Atualização Atividade 5

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d379f5b6d148573f55d533839f8191257ac70a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f56be74da143c8086a88487cb93b39a1cb593f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
